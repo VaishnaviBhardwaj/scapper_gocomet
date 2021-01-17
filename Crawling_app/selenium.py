75489c12187ad332f5aa0126c5d8c72ce5ff2a52
vaishnavibhardwaj <vaishnavibhardwaj27@gmail.com>
2021-01-17 12:15:55 +0530

    final

--- Crawling_app/selenium.py
+++ Crawling_app/selenium.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import os
+
 
 def Scrapping(str):
-
-	PATH ="C:\Program Files (x86)\chromedriver.exe"
-
-	driver =webdriver.Chrome(PATH)
+	
+	chrome_options = webdriver.ChromeOptions()
+	chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+	chrome_options.add_argument("--headless")
+	chrome_options.add_argument("--disable-dev-shm-usage")
+	chrome_options.add_argument("--no-sandbox")
+	driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 	driver.get("https://medium.com/search?q="+str) 
 	elements=driver.find_elements_by_class_name("graf--title")
