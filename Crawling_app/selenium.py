edf58506b4ee5b2755e53825144015b0d2d10361
vaishnavibhardwaj <vaishnavibhardwaj27@gmail.com>
2021-01-17 15:44:43 +0530

    change in selenium.py

--- Crawling_app/selenium.py
+++ Crawling_app/selenium.py
@@ -5,15 +5,13 @@ import os
 
 
 def Scrapping(str):
-    chrome_options = webdriver.ChromeOptions()
+	chrome_options = webdriver.ChromeOptions()
 	chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 	chrome_options.add_argument("--headless")
 	chrome_options.add_argument("--disable-dev-shm-usage")
 	chrome_options.add_argument("--no-sandbox")
 	driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-	# PATH ="C:\Program Files (x86)\chromedriver.exe"
 
-	# driver =webdriver.Chrome(PATH)
 
 	
 
